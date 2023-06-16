fc9cf3aa0fa8fe2d0bd58177e78fa57f19678ea2
Pranjal Rai <pranjalbhs@gmail.com>
2023-06-16 18:40:56 +0530

    done with tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./index.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <div className="text-3xl">Hello world</div>;
 }
 
 export default App;
